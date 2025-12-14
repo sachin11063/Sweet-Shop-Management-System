a54f3be66fde9c6ccec59ee61473ff8bb22c5a46
sachin11063 <22BCS11063@cuchd.in>
2025-12-14 14:36:39 +0530

    new changes in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
