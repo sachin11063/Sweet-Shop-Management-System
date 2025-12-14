cfffe3c489c73bb48a9931b01d299b38cc38b1f1
sachin11063 <22BCS11063@cuchd.in>
2025-12-14 14:27:10 +0530

    change in .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,12 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = 3000;
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
